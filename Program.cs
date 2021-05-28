cbda0254d3acbce288f8ab659afa5af7233159b0
Eduardo Gomez <edgome@microsoft.com>
2021-05-28 10:36:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+
+namespace TestVisionApp
+{
+    /// <summary>
+    /// https://docs.microsoft.com/en-us/azure/cognitive-services/computer-vision/quickstarts-sdk/client-library?tabs=visual-studio&pivots=programming-language-csharp#read-printed-and-handwritten-text
+    /// </summary>
+    class Program
+    {
+        private static IConfiguration _iconfiguration;        
+        static void Main(string[] args)
+        {
+            string imageUrl = args[0];
+            GetAppSettings();
+            Task t = ReadTextFromImage(imageUrl);
+            t.Wait();
+        }
+
+        public static async Task ReadTextFromImage(string imageUrl)
+        {
+            Console.WriteLine("Reading " + imageUrl);
+            string endpoint = _iconfiguration.GetSection("EndPoint").Value;
+            string subscriptionKey = _iconfiguration.GetSection("SubscriptionKey").Value;
+            var agent = new VisionAgent(endpoint, subscriptionKey);
+            var enumerable = agent.Read(imageUrl);
+            var enumerator = enumerable.GetAsyncEnumerator();
+            while (await enumerator.MoveNextAsync())
+            {
+                var item = enumerator.Current;
+                Console.WriteLine(item);            
+            }
+        }
+
+        static void GetAppSettings()
+        {            
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+            _iconfiguration = builder.Build();
+        }
+
+
+    }
+}
---
+++ VisionAgent.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TestVisionApp
+{
+    public class VisionAgent
+    {
+        ComputerVisionClient _client;
+        public VisionAgent(string endpoint, string subscriptionKey)
+        {
+            // Authenticate
+            _client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(subscriptionKey)) { Endpoint = endpoint };
+        }
+
+        public async IAsyncEnumerable<string> Read(string urlFile)
+        {
+            // Read text from URL
+            var textHeaders = await _client.ReadAsync(urlFile);
+            // After the request, get the operation operation ID
+            string operationLocation = textHeaders.OperationLocation;
+            //Thread.Sleep(2000);
+
+            // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+            // We only need the ID and not the full URL
+            const int numberOfCharsInOperationId = 36;
+            string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+            // Extract the text
+            ReadOperationResult results;            
+            do
+            {
+                results = await _client.GetReadResultAsync(Guid.Parse(operationId));
+            }
+            while ((results.Status == OperationStatusCodes.Running || results.Status == OperationStatusCodes.NotStarted));
+            var textUrlFileResults = results.AnalyzeResult.ReadResults;            
+            foreach (ReadResult page in textUrlFileResults)
+            {
+                foreach (Line line in page.Lines)
+                {
+                    yield return line.Text;
+                }
+            }           
+        }
+
+    }
+}
